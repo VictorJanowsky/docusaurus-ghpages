6aed8e7d71d9e9c205790b499ab3e40fa666db89
VictorJanowsky <a85236@ualg.pt>
2023-11-17 11:52:12 +0000

    alterei tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Victor Janowsky',
-  tagline: 'Aula ADC 17\11',
+  tagline: 'Aula ADC 17/11',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
